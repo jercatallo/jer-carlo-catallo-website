064f66d9f69255ac41cc62042ab70815abda96ca
Jer Catallo <jer.catallo@gmail.com>
2024-02-01 21:33:48 +0800

    chore: Pipeline update and added site responsiveness

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -12,8 +12,8 @@ function Navigation() {
   const screenSize = useScreenSize();
 
   function handleScroll() {
-    if (window.innerWidth >= TailwindConstants.ThemeScreens.md) {
-      if (timerRef.current !== undefined && window.innerWidth >= TailwindConstants.ThemeScreens.md) {
+    if (window.innerWidth >= TailwindConstants.ThemeScreens.lg) {
+      if (timerRef.current !== undefined && window.innerWidth >= TailwindConstants.ThemeScreens.lg) {
         const navigation = document.getElementById('navigation');
         if (navigation) {
           navigation.style.opacity = '0';
@@ -56,7 +56,7 @@ function Navigation() {
   useEffect(() => {
     const onScroll = () => handleScroll();
 
-    if (typeof window !== 'undefined' && document.readyState === 'complete' && screenSize.width >= TailwindConstants.ThemeScreens.md) {
+    if (typeof window !== 'undefined' && document.readyState === 'complete' && screenSize.width >= TailwindConstants.ThemeScreens.lg) {
       document.addEventListener('scroll', onScroll, true);
     } else {
       document.removeEventListener('scroll', onScroll);
@@ -71,7 +71,7 @@ function Navigation() {
   }, [screenSize]);
 
   const navigationTextColor = useMemo(() => {
-    if (screenSize.width >= TailwindConstants.ThemeScreens.md) {
+    if (screenSize.width >= TailwindConstants.ThemeScreens.lg) {
       if (scrollPosition === 'about') {
         return 'text-light';
       }
@@ -85,7 +85,7 @@ function Navigation() {
   }, [scrollPosition, screenSize]);
 
   const brandTextColor = useMemo(() => {
-    if (screenSize.width >= TailwindConstants.ThemeScreens.md) {
+    if (screenSize.width >= TailwindConstants.ThemeScreens.lg) {
       if (scrollPosition === 'about') {
         return 'text-light';
       }
@@ -104,25 +104,31 @@ function Navigation() {
           <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className={`font-heading ${navigationTextColor} self-center text-5xl whitespace-nowrap font-bold`}> jc  </span> <span className={`mx-5 font-heading  ${brandTextColor} self-center text-5xl whitespace-nowrap font-bold`}>;</span>
           </a>
-          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+          <button data-collapse-toggle="navbar-default" type="button" className="hidden inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
-              <li>
-                <a href="#" className={`block py-2 px-3 duration-500 ${navigationTextColor} bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0`} aria-current="page">Home</a>
+          <div className="hidden w-full lg:block lg:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-white">
+              <li onClick={()=> {
+                const element = document.getElementById("hero");
+                element?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+              }}>
+                <a href="#" className={`block py-2 px-3 duration-500 ${navigationTextColor} bg-blue-700 rounded lg:bg-transparent lg:text-blue-700 lg:p-0`} aria-current="page">Home</a>
+              </li>
+              <li onClick={()=> {
+                const element = document.getElementById("about");
+                element?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+              }}>
+                <a href="#" className={`block py-2 px-3 duration-500 ${navigationTextColor} rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0`}>About</a>
               </li>
               <li>
-                <a href="#" className={`block py-2 px-3 duration-500 ${navigationTextColor} rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0`}>About</a>
+                <a target='_blank' href="https://drive.google.com/file/d/1evTroPQysDvLXgTliyGe-n3UbZR6IVzs/view?usp=sharing" className={`block py-2 px-3 duration-500 ${navigationTextColor} rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0`}>Resume</a>
               </li>
               <li>
-                <a href="#" className={`block py-2 px-3 duration-500 ${navigationTextColor} rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0`}>Projects</a>
-              </li>
-              <li>
-                <a href="#" className={`block py-2 px-3 duration-500 ${navigationTextColor} rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0`}>Contact</a>
+                <a target='_blank' href="https://drive.google.com/file/d/1MiGGrTrHBLtBRhhuE1otrm23EaS5JUYM/view?usp=sharing" className={`block py-2 px-3 duration-500 ${navigationTextColor} rounded hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0`}>CV</a>
               </li>
             </ul>
           </div>
